7e3e40637fc4a898754f08e94dc4e50d8945afed
Piyush Chamoli <piyushchamoli11@gmail.com>
2021-12-31 21:36:38 +0530

    update

--- block-BJaadq/code/index.js
+++ block-BJaadq/code/index.js
@@ -1,5 +1,64 @@
 // NOTE: You can not use reduce methods to solve this exercise
 
+/*
+- 1. Write a function called `countAllPeople` which counts the total number of people in `got` variable defined in `data.js` file.
+
+- 2. Write a function called `peopleByHouses` which counts the total number of people in different houses in the `got` variable defined in `data.js` file.
+
+- 3. Write a function called `everyone` which returns a array of names of all the people in `got` variable.
+
+- 4. Write a function called `nameWithS` which returns a array of names of all the people in `got` variable whose name includes `s` or `S`.
+
+- 5. Write a function called `nameWithA` which returns a array of names of all the people in `got` variable whose name includes `a` or `A`.
+
+- 6. Write a function called `surnameWithS` which returns a array of names of all the people in `got` variable whoes surname is starting with `S`(capital s).
+
+- 7. Write a function called `surnameWithA` which returns a array of names of all the people in `got` variable whoes surname is starting with `A`(capital a).
+
+- 8. Write a function called `peopleNameOfAllHouses` which returns an object with the key of the name of house and value will be all the people in the house in an array.
+*/
+
+function everyone() {
+  let allNames = [];
+  got.houses.forEach(house => {
+    let peopleName = house.people.map(p => p.name);
+    allNames = allNames.concat(peopleName);
+  });
+  return allNames;
+}
+
+function nameWithS() {
+  let result = [];
+  result = result.concat(everyone.filter((person) => person.startsWith('s') || person.startsWith('S')));
+  return result;
+}
+
+function surnameWithS() {
+  let result = [];
+  function lastName(name) {
+    nameSplit = name.split(' ');
+    return nameSplit[1];
+  } 
+  result = result.concat(everyone.filter((person) => lastName(person)))
+  return result;
+}
+
+// {stark: [eddard, benajmin], lannister: [tywin, tyrion]}
+
+function peopleNameOfAllHouses() {
+  let result = {};
+  got.houses.forEach((house) => {
+    let names = [];
+    house.people.forEach((p) => {
+      names.push(p.name);
+    });
+    result[house.name] = names;
+  });
+  return result;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+/*
 function countAllPeople() {
   let totalCount = 0;
   got.houses.forEach(house => {
@@ -53,6 +112,7 @@ function peopleNameOfAllHouses() {
   });
   return final;
 }
+*/
 
 // Testing your result after writing your function
 console.log(countAllPeople());
