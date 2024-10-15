63c0089a23096640c3e274cbfd6877a1a4983f4f
Piyush Chamoli <piyushchamoli11@gmail.com>
2024-10-16 01:58:59 +0530

    update solution

--- block-BJaadl/code/index.js
+++ block-BJaadl/code/index.js
@@ -1,78 +1,82 @@
 let words = [
-  'mystery',
-  'brother',
-  'aviator',
-  'crocodile',
-  'pearl',
-  'orchard',
-  'crackpot',
-  'rhythm',
+  "mystery",
+  "brother",
+  "aviator",
+  "crocodile",
+  "pearl",
+  "orchard",
+  "crackpot",
+  "rhythm",
 ];
 
 // - Write a function findLongestWord that takes an array of words and returns the longest word from the array. (Use above array "words" to test it). If there are 2 with the same length, it should return the first occurrence.
 
 function findLongestWord(arr) {
-  return [...arr].sort((a,b) => a.length - b.length).pop();
+  let longestWord = arr[0];
+  words.forEach((word) => {
+    if (word.length > longestWord.length) {
+      longestWord = word;
+    }
+  });
+  return longestWord;
 }
-
-console.log(findLongestWord(words));
 
 // - Convert the above array "words" into an array of length of word instead of word.
 
-console.log(words.map((letter) => letter.length));
+console.log([...words].map((value) => value.length));
 
 // - Create a new array that only contains word with atleast one vowel.
 
-function checkVowel(word) {
-  return (word.includes('a') || 
-          word.includes('e') || 
-          word.includes('i') || 
-          word.includes('o') || 
-          word.includes('u'))
+function wordsWithVowel(word) {
+  return (
+    word.toLowerCase().includes("a") ||
+    word.includes("e") ||
+    word.includes("o") ||
+    word.includes("i") ||
+    word.includes("u")
+  );
 }
-
-let atleastOneVowel = words.filter((word) => checkVowel(word));
-
-console.log(atleastOneVowel);
+console.log([...words].filter(wordsWithVowel));
 
 // - Find the index of the word "rhythm"
 
-console.log(words.indexOf('rhythm'));
-
-words.findIndex((w) => w == 'rhythm');
+console.log([...words].findIndex((val) => val === "rhythm"));
 
 // - Create a new array that contians words not starting with vowel.
 
-let notStartWithVowel = words.filter((word) => !checkVowel(word[0]));
+console.log(
+  [...words].filter((word) => {
+    return !(
+      word.startsWith("a") ||
+      word.startsWith("e") ||
+      word.startsWith("i") ||
+      word.startsWith("o") ||
+      word.startsWith("u")
+    );
+  })
+);
 
 // - Create a new array that contianse words not ending with vowel
 
-let notEndWithVowel = words.filter((word) => !checkVowel(word[word.length - 1]));
-
-let numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
-
 // - Create a sumArray function that takes an array of number as a parameter, and calculate the sum of all its numbers
 
-function sumArray(array) {
-  return array.reduce((acc, cv) => {
-    acc = acc + cv;
-    return acc;
-  }, 0);
+function sumArray(arr) {
+  let total = 0;
+  arr.forEach((num) => total + num);
+  return total;
 }
 
 // - Make a new array that contains number multiplied by 3 like [6, 18, 27 ...]
 
-let multiplyByThree = numbers.filter((num) => num * 3);
-
 // - Create a new array that contains only even numbers
 
-let evenArray = numbers.filter((num) => num % 2 === 0);
-console.log(evenArray);
+// let evenArray = numbers.filter((num) => num % 2 === 0);
+// console.log(evenArray);
 
 // - Create  a new array that contains only odd numbers.
 
-let oddArray = numbers.filter((num) => num%2!==0);
-console.log(oddArray);
+// let oddArray = numbers.filter((num) => num % 2 !== 0);
+// console.log(oddArray);
 
 // - Create a new array that should have true for even number and false for odd numbers.
 
@@ -80,24 +84,19 @@ let oddOrEven = numbers.map((num) => num % 2 === 0);
 
 // - Sort the above number in assending order.
 
-let sortedNumbers = numbers.sort((a,b) => a - b);
+let sortedNumbers = numbers.sort((a, b) => a - b);
 
 // - Does sort mutate the original array?
 
-//Yes
+Yes;
 
 // - Find the sum of the numbers in the array.
-
-let sum = numbers.reduce((acc,cv) => {
-  acc = acc + cv;
-  return acc;
-},0)
 
 //- Write a function averageNumbers that receives an array of numbers and calculate the average of the numbers
 
 function averageNumbers(array) {
   return (
-    array.reduce((acc,cv) => {
+    array.reduce((acc, cv) => {
       acc = acc + cv;
       return acc;
     }, 0) / array.length
@@ -105,26 +104,20 @@ function averageNumbers(array) {
 }
 
 let strings = [
-  'seat',
-  'correspond',
-  'linen',
-  'motif',
-  'hole',
-  'smell',
-  'smart',
-  'chaos',
-  'fuel',
-  'palace',
+  "seat",
+  "correspond",
+  "linen",
+  "motif",
+  "hole",
+  "smell",
+  "smart",
+  "chaos",
+  "fuel",
+  "palace",
 ];
 
 // - Write a function averageWordLength that receives an array of words2 and calculate the average length of the words.
 
-function averageWordLength(array) {
-  return (
-    array.map((w) => w.length)
-    .reduce((acc,cv) => {
-      acc = acc + cv;
-      return acc;
-    },0) / words.length
-  )
+function averageWordLength(arr) {
+  return arr.reduce((acc, cv) => acc + cv.length, 0) / arr.length;
 }
